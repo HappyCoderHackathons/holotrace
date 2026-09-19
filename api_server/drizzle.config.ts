fecf1ce5c761ddb683979fceb45ef7423e98d42f
Ender <nallahabi2003@gmail.com>
2026-09-19 16:24:51 -0400

    Modified dbCredentials section of drizzle.config.ts

--- api_server/drizzle.config.ts
+++ api_server/drizzle.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
+        host: process.env.PG_HOST!,
+        port: 5432,
+        user: process.env.PG_USER!,
+        password: process.env.PG_PASSWORD!,
+        database: process.env.PG_DATABASE!,
+        ssl: false
+    }
 });
